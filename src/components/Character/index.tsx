be3da433112ee913e92e221c2f3c0b35905b7050
Aman Yadav <amanyadavji9099@gmai.com>
2026-07-19 19:32:57 +0530

    Fix unused imports and variables to resolve compilation errors

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,8 +1,6 @@
 import Scene from "./Scene";
 
 const CharacterModel = () => {
-  // Set to true to use the static 2D image, set to false to use the interactive 3D model
-  const useStaticImage = false;
   return <Scene />;
 };
 
